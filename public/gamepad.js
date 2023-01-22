2faf93c9cbbfd11b776997ff4af122385c178c83
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-01-22 22:33:55 +0100

    feat: vtt ready

--- public/gamepad.js
+++ public/gamepad.js
@@ -34,6 +34,7 @@ function gamepadTrigger() {
         else if (0 == index) Reveal.prev(); // B
         else if (6 == index) Reveal.next(); // L2
         else if (7 == index) Reveal.prev(); // R2
+        else if (8 == index) window.zoom(); // select
       }
     }
   }
